9723ca675601fe1fcfcf9df0498fbcf5c2aff8f2
QuirinEcker <quirin.ecker@bajupa.com>
2017-11-21 10:47:18 +0100

    break

--- src/main/java/Ecker_TurmRechnen.java
+++ src/main/java/Ecker_TurmRechnen.java
@@ -5,6 +5,14 @@ public class Ecker_TurmRechnen {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int ganzeZahl;
+        int multiplikation;
 
+        System.out.print("Geben sie eine Ganze Zahl ein: ");
+        ganzeZahl = scanner.nextInt();
+
+        for (int i = 0 ; i <= 10;){
+            System.out.printf("%30d * 2");
+        }
     }
 }
